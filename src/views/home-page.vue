7ad3f8a86d7d5df864ca9d04c90e951698547ab4
zhemga <andreaconte.samp@gmail.com>
2023-12-20 15:00:56 +0200

    add homepage

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,11 +1,26 @@
 <template lang="">
+    <center>
     <div>
-        
+        <div>
+    <h1>Welcome to the Auto Parts Accounting Manager VUE UI</h1>
+    <p>Explore our products and categories:</p>
+    
+    <h2>
+    <router-link to="auto-parts">
+        Go to Auto Parts
+    </router-link>
+    <br>
+    <router-link to="/auto-part-categories">
+        Go to Auto Part Categories
+    </router-link>
+</h2>
+  </div>
     </div>
+</center>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style lang="">
